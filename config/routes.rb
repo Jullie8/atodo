c1e7837b3f08b12ff48a3550af6d1333523907c3
github username <github email address>
2019-01-03 09:58:34 -0500

    add log out feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get "/pages/:page" => "pages#show"
   root "pages#show", page: "home"
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :tasks
   resources :usertodos
   resources :todos
