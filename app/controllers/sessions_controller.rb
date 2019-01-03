ec8386d97e6bf779be4b06afc3626984fc3dbdb8
github username <github email address>
2019-01-02 20:59:01 -0500

    basic login authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+  def new
+  
+  end
+
+  def create
+    # user = User.find_by(email: params[:session][:email].downcase)
+    # if user && user.authenticate(params[:session][:password])
+
+    user = User.find_by(email: params[:email].downcase)
+  
+    if user && user.authenticate(params[:password])
+      # Log the user in and redirect to the user's show page.
+      log_in! user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+    # Logs out the current user.
+
+    def destroy 
+    session.delete(:user_id)
+    @current_user = nil
+    redirect_to root_url
+    end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,10 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-
+       
         if @user.valid?
             @user.save
+            log_in! @user
             flash[:success] = "Welcome to the A Todo App!"
             redirect_to @user
         else 
